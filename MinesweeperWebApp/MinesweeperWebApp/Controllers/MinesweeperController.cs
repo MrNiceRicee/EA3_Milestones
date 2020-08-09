165be9adbae19d9779ac3359cba45331f2ec55c8
MrNiceRicee <47019696+MrNiceRicee@users.noreply.github.com>
2020-08-09 12:52:21 -0700

    update

--- MinesweeperWebApp/MinesweeperWebApp/Controllers/MinesweeperController.cs
+++ MinesweeperWebApp/MinesweeperWebApp/Controllers/MinesweeperController.cs
@@ -1,8 +1,11 @@
 ﻿using MinesweeperWebApp.Models;
 using MinesweeperWebApp.Services.Business;
 using MinesweeperWebApp.Services.Business.Data;
+using MinesweeperWebApp.Services.Utilities;
+using NLog;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,21 +17,35 @@ namespace MinesweeperWebApp.Controllers
         // GET: Minesweeper
         static Board gameBoard = new Board();
         static string username = "";
+
         public ActionResult Index()
-        {
+        {       
             username = "";
             return View("Login");
         }
 
         public ActionResult Login(User user)
         {
-            SecurityServices securityServices = new SecurityServices();
-            if (securityServices.Authenticate(user))
+            //put an item in the log
+            MyLogger.GetInstance().Warning("Starting login controller.");
+            try
             {
-                username = user.Username;
-                return View("MainUserScreen", user);
+                MyLogger.GetInstance().Info("Entering the login controller. Login Methoid");
+
+                SecurityServices securityServices = new SecurityServices();
+                if (securityServices.Authenticate(user))    //check if login was successful
+                {
+                    MyLogger.GetInstance().Info("Exit login controller. Login success!");
+                    username = user.Username;
+                    return View("MainUserScreen", user);
+                }
+                return View("Login");
             }
-            return View("Login");
+            catch (Exception e)
+            {
+                MyLogger.GetInstance().Error("Exception! "+ e.Message);
+                return Content("Exception in Login" + e.Message);
+            }
         }
 
         public ActionResult ProcessANewRegister(User user)
@@ -50,8 +67,10 @@ namespace MinesweeperWebApp.Controllers
             return View("GameScreen", gameBoard);
         }
 
+        [HttpPost]
         public ActionResult HandleButtonClick(string mine)
         {
+            Debug.WriteLine("update: "+mine);
             string[] info = mine.Split(',');
             int x = int.Parse(info[0]);
             int y = int.Parse(info[1]);
@@ -64,7 +83,7 @@ namespace MinesweeperWebApp.Controllers
                     gameBoard.revealNearbyZero(gameBoard.getCell(x, y));
                 }
             }
-            return View("GameScreen",gameBoard);
+            return PartialView("_GameField",gameBoard);
         }
 
 
---
+++ MinesweeperWebApp/MinesweeperWebApp/Services/Utilities/MyLogger.cs
@@ -0,0 +1,96 @@
+﻿using NLog;
+using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinesweeperWebApp.Services.Utilities
+{
+    public class MyLogger : ILogger
+    {
+        // Singleton pattern example. Only one instance of this class can be instanciated
+
+        private static MyLogger instance; //single ton design apttern. single instance of this class.
+        private static Logger logger; // static variable to hold a single instance of the nLogger
+
+        //single design pattern - private constructor
+        private MyLogger()
+        {
+
+        }
+
+        // single design pattern shown here - this function creates an instance of the class if it has not yet been instancieted. If the class already 
+        // exist in the program, then send the mto the reference to the original
+        public static MyLogger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MyLogger();
+            }
+            return instance;
+        }
+
+        private Logger GetLogger(string theLogger)
+        {
+            if (MyLogger.logger == null)
+            {
+                MyLogger.logger = LogManager.GetLogger(theLogger);
+            }
+            return MyLogger.logger;
+        }
+
+        public void Debug(string message, string arg = null)
+        {
+            if (arg==null)
+            {
+                GetLogger("myAppLoggerRules").Debug(message);
+            }
+            else
+            {
+                GetLogger("myAppLoggerRules").Debug(message,arg);
+            }
+        }
+
+        public void Error(string message, string arg = null)
+        {
+            if (arg == null)
+            {
+                GetLogger("myAppLoggerRules").Error(message);
+            }
+            else
+            {
+                GetLogger("myAppLoggerRules").Error(message, arg);
+            }
+        }
+
+       
+
+        public void Info(string message, string arg = null)
+        {
+            if (arg == null)
+            {
+                GetLogger("myAppLoggerRules").Info(message);
+            }
+            else
+            {
+                GetLogger("myAppLoggerRules").Info(message, arg);
+            }
+        }
+ 
+
+        public void Warning(string message, string arg = null)
+        {
+            if (arg == null)
+            {
+                GetLogger("myAppLoggerRules").Warn(message);
+            }
+            else
+            {
+                GetLogger("myAppLoggerRules").Warn(message, arg);
+            }
+        }
+
+       
+    }
+}
